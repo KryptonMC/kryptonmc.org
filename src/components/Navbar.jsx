75a72a577021a83d1efe8c494eef44328ee1851f
Callum Seabrook <callum.seabrook@prevarinite.com>
2021-09-19 11:02:52 +0100

    Fixed window not available during build

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,22 +16,23 @@ export default class Navbar extends Component {
     }
 
     render() {
+        const width = typeof window === "undefined" ? 0 : window.innerWidth;
         return (
             <div className="navbar">
                 <span>
                     <img
-                        src={window.innerWidth <= 650 ? KryptonLogo : KryptonBanner}
+                        src={width <= 650 ? KryptonLogo : KryptonBanner}
                         alt="Krypton"
                         width="200px"
                         height="40px"
-                        onClick={window.innerWidth <= 650 && this.toggle}
+                        onClick={width <= 650 && this.toggle}
                     />
-                    {window.innerWidth <= 650 && <i className="fas fa-caret-down" onClick={this.toggle} />}
-                    {window.innerWidth <= 650 && <span className={`toggle-container ${this.state.toggled ? "on" : "off"}`}>
+                    {width <= 650 && <i className="fas fa-caret-down" onClick={this.toggle} />}
+                    {width <= 650 && <span className={`toggle-container ${this.state.toggled ? "on" : "off"}`}>
                         {InternalLinks.map(link => (<NavbarLink {...link} />))}
                     </span>}
                 </span>
-                {window.innerWidth > 650 && <div className="navbar-right">
+                {width > 650 && <div className="navbar-right">
                     {ExternalLinks.map(link => (<NavbarLink {...link} />))}
                 </div>}
             </div>
